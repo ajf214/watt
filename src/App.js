f211df4974ac9f2ec20fba031fe388667acb6d56
Alex Frankel <alex.j.frankel@gmail.com>
2018-01-15 06:27:43 -0800

    have a dynamic autocomplete box on the homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,123 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './autosuggest.css';
+import Autosuggest from 'react-autosuggest';
 
-class App extends Component {
+const people = [
+  {
+    first: 'Charlie',
+    last: 'Brown',
+    twitter: 'dancounsell'
+  },
+  {
+    first: 'Charlotte',
+    last: 'White',
+    twitter: 'mtnmissy'
+  },
+  {
+    first: 'Chloe',
+    last: 'Jones',
+    twitter: 'ladylexy'
+  },
+  {
+    first: 'Cooper',
+    last: 'King',
+    twitter: 'steveodom'
+  }
+];
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('\\b' + escapedValue, 'i');
+  
+  return people.filter(person => regex.test(getSuggestionValue(person)));
+}
+
+function getSuggestionValue(suggestion) {
+  return `${suggestion.first} ${suggestion.last}`;
+}
+
+function renderSuggestion(suggestion, { query }) {
+  const suggestionText = `${suggestion.first} ${suggestion.last}`;
+
+  var AutosuggestHighlightMatch = require('autosuggest-highlight/match');
+  var AutosuggestHighlightParse = require('autosuggest-highlight/parse');
+
+  const matches = AutosuggestHighlightMatch(suggestionText, query);
+  const parts = AutosuggestHighlightParse(suggestionText, matches);
+
+  return (
+    <span className={'suggestion-content ' + suggestion.twitter}>
+      <span className="name">
+        {
+          parts.map((part, index) => {
+            const className = part.highlight ? 'highlight' : null;
+
+            return (
+              <span className={className} key={index}>{part.text}</span>
+            );
+          })
+        }
+      </span>
+    </span>
+  );
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };    
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+  
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Type 'c'",
+      value,
+      onChange: this.onChange
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Autosuggest 
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps} />
     );
   }
 }
